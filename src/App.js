152706d8f2a7dd195affdbbcb10ae4e37c84919b
AliBaniJaber <60363242+AliBaniJaber@users.noreply.github.com>
2020-03-11 22:09:17 +0200

    props+state
    11/3/2020 10:25pm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React,{Component} from 'react';
 import ComponentReact from './component/functioncomponent';
 import Student from './component/student';
+import "./css/Student.css";
+import FormStudent from "./component/formStudent";
+
+
 
 function aliattr(){
  return <p> ali test function attr</p>
@@ -19,9 +23,13 @@ class App extends Component{
  render() {
    return (
        <div>
+       <div className="Student">
            <Student ID="1" name="ali" BD="1-17-1999"> </Student>
            <Student ID="1" name="ali" BD="1-17-1999"> </Student>
            <Student ID="1" name="ali" BD="1-17-1999"> </Student>
+       </div>
+           <FormStudent/>
+
        </div>
 
    );
--- src/component/student.js
+++ src/component/student.js
@@ -2,6 +2,13 @@ import React,{Component} from "react";
 import "./css/student.css";
 class student extends Component{
 
+    constructor(props) {
+        super(props);
+        this.incremant=this.incremant.bind(this);
+        this.changename=this.changename.bind(this);
+    }
+
+
     state=
         {
             age:20,
@@ -18,10 +25,22 @@ class student extends Component{
             ]
 
 
-
-
-
         };
+
+     incremant ()
+     {
+         this.setState({age :this.state.age+1});
+     }
+    changename(event)
+    {
+        const newname=event.target.value;
+        this.setState({user:{
+            ...this.state.user,
+            firstname: newname
+        }
+        });
+
+    }
 
 
 
@@ -32,6 +51,10 @@ class student extends Component{
         age:  {this.state.age}  firsname:{this.state.user.firstname }  lasrname:{this.state.user.lastNAME}
         <br/>
             socialLinks:{this.state.socialLinks[0] } {this.state.socialLinks[1] }
+
+
+            <button onClick={this.incremant}>incremant age</button>
+            <input type="text" onChange={this.changename}/>
 
 
         </div>
