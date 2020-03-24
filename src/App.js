8e6a28f3207519dad6bf17357a48ffa27e6c6d3e
AliBaniJaber <60363242+AliBaniJaber@users.noreply.github.com>
2020-03-24 02:15:23 +0200

    delete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ALI from './component/nav';
-import home from "./component/Home";
-import {BrowserRouter , Route} from 'react-router-dom';
-import about from "./component/about";
-import blok from './component/blolk';
+
 class App extends Component{
  render() {
    return (
-       <BrowserRouter>
            <div className="App">
-               <ALI/>
-               <Route exact path="/74" component={home}/>
-               <Route  path="/about" component={about}/>
-               <Route  path="/blok" component={blok}/>
+            welcom
            </div>
-       </BrowserRouter>
+
 
    );
  }
--- src/component/functioncomponent.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from "react";
-
-
-
-function ComponentReact(props) {
-    return <div>the attr name=  {props.name} {props.fun}    from function  </div>
-}
-export default ComponentReact;
-
